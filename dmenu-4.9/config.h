b8961d11a8bb889942191f38b0f0970597cf3d13
cashew69 <92780393+cashew69@users.noreply.github.com>
2024-01-10 00:35:47 +0530

    Update config.h

--- dmenu-4.9/config.h
+++ dmenu-4.9/config.h
@@ -7,7 +7,7 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-#include "/home/cashew/.cache/wal/colors-wal-dmenu.h"
+#include "colors-wal-dmenu.h"
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
