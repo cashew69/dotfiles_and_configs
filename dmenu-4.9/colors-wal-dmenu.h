5744ae7b51c511739000586cc8fd9bf46fcafcdb
cashew69 <92780393+cashew69@users.noreply.github.com>
2024-01-10 00:35:20 +0530

    Add files via upload

---
+++ dmenu-4.9/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#b3b8cf", "#0d0f12" },
+	[SchemeSel] = { "#b3b8cf", "#946233" },
+	[SchemeOut] = { "#b3b8cf", "#56639F" },
+};
